bb372c01e98bae4812b8ae0c1b045db7d875da20
arys-training <103812158+arys-training@users.noreply.github.com>
2022-04-21 09:38:26 +0600

    Sleep time is updated as per task requirement

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     try:
         assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     except:
